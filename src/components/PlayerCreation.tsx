46a5cc519391f3b27399508e39a7a18d4054891c
MagoroGraphics <magoro.graphics@gmail.com>
2023-01-11 11:41:59 +0000

    Adds states for player list and form stage

--- src/components/PlayerCreation.tsx
+++ src/components/PlayerCreation.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React , {useState} from "react";
 import { Player } from "../types/PlayerType";
 
 const PlayerCreation = () => {
 
+const [stage, setStage] = useState("PlayerNumberInput")
+const [players, setPlayers] = useState({})
+
+    //Create an option to introduce number of players
+    //Create a state to know which stage the app is in
+    //Based on number of players, display input for player names
+    //Create a state with a players object
+    //Post players to database
+
     return(
         <div>
             <h1>Player Creation</h1>
+            {stage === "PlayerNumberInput" && <form><h2>How many players?</h2><input value=""></input></form>}
+            {stage === "PlayerCreation" && <h3>Create Players</h3>}
         </div>
     )
 }
