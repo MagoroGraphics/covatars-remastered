9b12a4dcb75d87439a1c237d96e7a5cfdb9e1894
MagoroGraphics <magoro.graphics@gmail.com>
2023-01-25 17:22:08 +0000

    Adds styling to general app

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,26 @@ import React, {useState} from 'react';
 import './App.css';
 import PlayerCreation from './components/PlayerCreation';
 import Game from './containers/Game';
+import styled from 'styled-components';
 
 
 function App() {
 
+  const MainAppWrapper = styled.div`
+      position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-image: url(icons/background_tile.png);
+  ` 
   const [isGameStarted, setIsGameStarted] = useState(false)
 
   return (
-    <div className="App">
+    <MainAppWrapper className="App">
       {!isGameStarted && <PlayerCreation setIsGameStarted={setIsGameStarted}/>}
       {isGameStarted && <Game/>}
-    </div>
+    </MainAppWrapper>
   );
 }
 
