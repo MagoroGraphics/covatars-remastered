f67b80cb25423c0064f7c29be99db07d80b2099b
MagoroGraphics <magoro.graphics@gmail.com>
2023-01-25 17:22:31 +0000

    Adds ability to set all players in state

--- src/components/PlayerInput.tsx
+++ src/components/PlayerInput.tsx
@@ -1,34 +1,33 @@
-import { Player } from "../types/PlayerType"
-import React, {useState, SetStateAction} from "react"
+import { Player } from "../types/PlayerType";
+import React, { useState, SetStateAction } from "react";
 
 interface PlayerInputProps {
-    playerNum: number
-    players: string[]
-    setPlayers: (players: string[]) => void
+  playerNum: number;
+  players: string[];
+  setPlayers: (players: string[]) => void;
 }
 
-const PlayerInput = ({playerNum, players, setPlayers}:PlayerInputProps) => {
-    const [playerName, setPlayerName] = useState("")
-const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+const PlayerInput = ({ playerNum, players, setPlayers }: PlayerInputProps) => {
+  const [playerName, setPlayerName] = useState("");
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target: string = e.target.value;
-    setPlayerName(target)
-}
+    setPlayerName(target);
+  };
 
-const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    let playerList = [...players]
-    playerList.push(playerName)
-    setPlayers(playerList)
-}
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    let playerList = [...players];
+    playerList.push(playerName);
+    return setPlayers(playerList);
+  };
 
-return (
+  return (
     <form>
-        <label>Player {playerNum+1}</label>
-        <input onChange={handleOnChange}>
-        </input>
-        <button onClick={()=>handleSubmit}>Create</button>
+      <label>Player {playerNum + 1}</label>
+      <input onChange={handleOnChange}></input>
+      <button onClick={handleSubmit}>Create</button>
     </form>
-)
-}
+  );
+};
 
-export default PlayerInput
+export default PlayerInput;
