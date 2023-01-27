94346698c9be219614969458fe0333f8ea354e54
MagoroGraphics <magoro.graphics@gmail.com>
2023-01-27 12:03:58 +0000

    Adds more styling to instructions button

--- src/components/InstructionsButton.tsx
+++ src/components/InstructionsButton.tsx
@@ -1,14 +1,26 @@
 import styled from "styled-components";
 
 const InstructionsButtonStyled = styled.button`
-    color: yellow;
-    height: 10vh;
-    width: auto;
-    cursor:pointer;
-  `;
+  color: yellow;
+  height: 10vh;
+  width: auto;
+  cursor: pointer;
+  background-color:rgba(0,0,0,0);
+  border:none;
+  font-weight: bold;
+  font-size:2vh;
+  font-family: 'Lobster';
+`;
+
+
 
 const InstructionsButton = () => {
-    return <InstructionsButtonStyled><img src="icons/instructions_icon.svg"/>Instructions</InstructionsButtonStyled>
-}
+  return (
+    <InstructionsButtonStyled>
+      <img src="icons/instructions_icon.svg" />
+      Instructions
+    </InstructionsButtonStyled>
+  );
+};
 
-export default InstructionsButton
+export default InstructionsButton;
