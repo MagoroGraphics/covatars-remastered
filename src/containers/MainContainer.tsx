77c974639e58ffa292519f6723fff3a03c32122b
MagoroGraphics <magoro.graphics@gmail.com>
2023-03-06 17:02:18 +0000

    Adds a main container

---
+++ src/containers/MainContainer.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Modal from "../components/Modal";
+import PlayerCreation from "../components/PlayerCreation";
+import Game from "./Game";
+import Instructions from "../components/Instructions";
+
+const MainContainer = () => {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+  return (
+    <>
+      <Modal isOpen={isModalOpen} onClose={toggleModal}>
+        <Instructions />
+      </Modal>
+      {!isGameStarted && <PlayerCreation setIsGameStarted={setIsGameStarted} />}
+      {isGameStarted && <Game />}
+    </>
+  );
+};
+
+export default MainContainer;
