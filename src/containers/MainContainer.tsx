5a275df2f97a1dec72fdc63fa93d51acd7aa8150
MagoroGraphics <magoro.graphics@gmail.com>
2024-01-18 15:40:21 +0000

    Create simple instructions button

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -3,22 +3,40 @@ import Modal from "../components/Modal";
 import PlayerCreation from "../components/PlayerCreation";
 import Game from "./Game";
 import Instructions from "../components/Instructions";
+import styled from "styled-components";
+
+const InstructionsButtonStyled = styled.button`
+  color: yellow;
+  height: 10vh;
+  width: auto;
+  cursor: pointer;
+  background-color:rgba(0,0,0,0);
+  border:none;
+  font-weight: bold;
+  font-size:2vh;
+  font-family: 'Lobster';
+`;
 
 const MainContainer = () => {
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const toggleModal = () => {
+  const toggleInstructionsModal = () => {
     setIsModalOpen(!isModalOpen);
   };
 
   return (
     <>
-      {isModalOpen && <Modal isOpen={isModalOpen} onClose={toggleModal}>
-        <Instructions />
-      </Modal>}
-      {!isGameStarted && <PlayerCreation setIsGameStarted={setIsGameStarted} />}
-      {isGameStarted && <Game />}
+      <section>
+        <InstructionsButtonStyled onClick={toggleInstructionsModal}>Instructions</InstructionsButtonStyled>
+      </section>
+      {isModalOpen && (
+        <Modal isOpen={isModalOpen} onClose={toggleInstructionsModal}>
+          <Instructions />
+        </Modal>
+      )}
+      {/* {!isGameStarted && <PlayerCreation setIsGameStarted={setIsGameStarted} />}
+      {isGameStarted && <Game />} */}
     </>
   );
 };
