f3cad12449d398af112884319f2c5fbdb89930c5
MagoroGraphics <magoro.graphics@gmail.com>
2024-01-23 17:58:50 +0000

    Fix instructions modal display

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,8 +14,8 @@ const CloseButton = styled.div`
   top: 0;
   right: 0;
   background-color: purple;
-  width:fit-content;
-  height:auto;
+  width: fit-content;
+  height: auto;
   padding: 5px 10px;
   color: yellow;
   margin: 24px;
@@ -23,21 +23,25 @@ const CloseButton = styled.div`
 `;
 
 const ModalOverlay = styled.div`
-  background-color: 0 0 0 0.75;
+  background-color: black;
+  opacity: 75%;
   position: absolute;
   top: 0px;
   right: 0px;
   bottom: 0px;
   left: 0px;
   z-index: 1;
+  height: 100%;
+  width: 100%;
 `;
 
 const ModalBox = styled.div`
   background-color: #fcfc97;
   z-index: 1000;
+  left:25%;
   width: 50%;
   height: 50%;
-  position: relative;
+  position: absolute;
   color: black;
   border-radius: 25px;
   padding: 50px;
