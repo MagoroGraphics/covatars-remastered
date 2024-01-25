c6a293984a50f89e0e2b14716287f4b42618e166
MagoroGraphics <magoro.graphics@gmail.com>
2024-01-25 19:07:26 +0000

    Add styled component for player hand

--- src/containers/Game.tsx
+++ src/containers/Game.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const PlayerHand = styled.div`
+  height:20%;
+  width: 90%;
+  background-color: gray;
+  border-radius: 1vh;
+  border: solid;
+  margin-left: auto;
+  margin-right: auto;
+`
 
 const Game = () => {
   return (
     <>
-      <h2>This is the game board</h2>
+      <PlayerHand/>
     </>
   );
 };
